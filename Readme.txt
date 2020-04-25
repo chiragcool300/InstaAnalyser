d93625608daedd961e74d183983cdd974f4b9850
Chirag Choudhary <chiragcool300@gmail.com>
2020-04-25 11:27:20 +0530

    first commit

---
+++ Readme.txt
@@ -0,0 +1,22 @@
+Hello.
+
+Instructions....
+
+
+1.Please check your Internet Connection before using this website.
+2.Please make sure you have a server as PHP files not run without it.You can run it on localhost of WAMP or LAMP.
+3.Please provide correct username in index.html
+4.steps--->    
+           1 Open index.html
+           2 Type your Username
+           3 See your stats..
+           4 For complete profile click on see complete profile.
+
+
+
+This is just the basic layout I have present before you.So you can see the functionality and UI part
+
+
+
+
+Thank You     
---
+++ index.php
@@ -0,0 +1,224 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Selecao Bootstrap Template - Index</title>
+  <meta content="" name="descriptison">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/line-awesome/css/line-awesome.min.css" rel="stylesheet">
+  <link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
+  <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Selecao - v2.1.0
+  * Template URL: https://bootstrapmade.com/selecao-bootstrap-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top d-flex align-items-center   ">
+    <div class="container d-flex align-items-center">
+
+      <div class="logo mr-auto">
+        <h1 class="text-light"><a href="index.html">Insta Analyser</a></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+      </div>
+
+      <nav class="nav-menu d-none d-lg-block">
+        <ul>
+          <li class="active"><a href="index.html">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#services">Services</a></li>
+          <li><a href="#portfolio">Portfolio</a></li>
+          <li><a href="#pricing">Pricing</a></li>
+          <li><a href="#team">Team</a></li>
+          <li class="drop-down"><a href="">Drop Down</a>
+            <ul>
+              <li><a href="#">Drop Down 1</a></li>
+              <li class="drop-down"><a href="#">Drop Down 2</a>
+                <ul>
+                  <li><a href="#">Deep Drop Down 1</a></li>
+                  <li><a href="#">Deep Drop Down 2</a></li>
+                  <li><a href="#">Deep Drop Down 3</a></li>
+                  <li><a href="#">Deep Drop Down 4</a></li>
+                  <li><a href="#">Deep Drop Down 5</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Drop Down 3</a></li>
+              <li><a href="#">Drop Down 4</a></li>
+              <li><a href="#">Drop Down 5</a></li>
+            </ul>
+          </li>
+          <li><a href="#contact">Contact</a></li>
+
+        </ul>
+      </nav><!-- .nav-menu -->
+
+    </div>
+  </header><!-- End Header -->
+
+ 
+
+    <!-- ======= Testimonials Section ======= -->
+    <section id="testimonials" class="testimonials">
+      <div class="container">
+
+        <div class="section-title" data-aos="zoom-out">
+          <h2>Analyser</h2>
+          <p>Your Analysed Profile</p>
+        </div>
+
+            <div class="row">
+
+          <div class="col-lg-4 col-md-6 d-flex  align-items-stretch " style="text-align:center;">
+
+         
+</div>
+      <div class="col-lg-4 col-md-6 d-flex  align-items-stretch " style="text-align:center;">
+
+          <div class="testimonial-item">
+    <img src="" class="profile-pic" alt="" style="border-radius:50%;">
+
+            <h3 class="username"></h3>
+
+            <h4 class="name"></h4>
+
+           <span class="followers"></span> followers
+
+<br>
+  <span class="following"></span> following
+<br><br>  
+Engagement-rate :- <span class="engagement"></span>%
+<h4 class="biography"></h4>
+   <br>
+
+<br>
+
+<button type="button" class="btn btn-primary"><a class="text-white" href="Task.php/<?php $x=$_GET['user'];echo "?user=$x";?>">See Complete Profile</a></button><br><br>
+        
+          </div>
+</div>
+      <div class="col-lg-4 col-md-6 d-flex  align-items-stretch " style="text-align:center;">
+
+    
+</div>
+
+     
+       
+
+    
+           
+
+      </div>
+    </section><!-- End Testimonials Section -->
+
+  
+ 
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+    <div class="container">
+      <h3>Chirag Instagram Analyser</h3>
+      <p>Et aut eum quis fuga eos sunt ipsa nihil. Labore corporis magni eligendi fuga maxime saepe commodi placeat.</p>
+      <div class="social-links">
+        <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+        <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+        <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+        <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+        <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+      </div>
+ 
+ 
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top"><i class="ri-arrow-up-line"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/jquery/jquery.min.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/venobox/venobox.min.js"></script>
+  <script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
+<script>
+  function nFormatter(num){
+    if(num >= 1000000000){
+      return (num/1000000000).toFixed(1).replace(/\.0$/,'') + 'G';
+    }
+    if(num >= 1000000){
+      return (num/1000000).toFixed(1).replace(/\.0$/,'') + 'M';
+    }
+    if(num >= 1000){
+      return (num/1000).toFixed(1).replace(/\.0$/,'') + 'K';
+    }
+    return num;
+  }
+
+
+  $.ajax({
+    url:"https://www.instagram.com/<?php echo $_GET['user'];?>?__a=1",
+    type:'get',
+    success:function(response){
+      $(".profile-pic").attr('src',response.graphql.user.profile_pic_url);
+      $(".name").html(response.graphql.user.full_name);
+      $(".biography").html(response.graphql.user.biography);
+      $(".username").html(response.graphql.user.username);
+      $(".number-of-posts").html(response.graphql.user.edge_owner_to_timeline_media.count);
+      $(".followers").html(nFormatter(response.graphql.user.edge_followed_by.count));
+      $(".following").html(nFormatter(response.graphql.user.edge_follow.count));
+      posts = response.graphql.user.edge_owner_to_timeline_media.edges;
+      posts_html = '';
+      var count=0;
+      
+      for(var i=0;i<posts.length;i++){
+        url = posts[i].node.display_url;
+        likes = posts[i].node.edge_liked_by.count;
+        comments = posts[i].node.edge_media_to_comment.count;
+        count=count+likes+comments;
+       
+        posts_html += '<div class="col-md-4 equal-height"><img style="min-height:50px;background-color:#fff;width:100%" src="'+url+'"><div class="row like-comment"><div class="col-md-6">'+nFormatter(likes)+' LIKES</div><div class="col-md-6">'+nFormatter(comments)+' COMMENTS</div></div></div>';
+      }
+         count=count/posts.length;
+        
+           $(".engagement").html(nFormatter((count/(response.graphql.user.edge_followed_by.count))*100).toFixed(3));
+      $(".posts").html(posts_html);
+    }
+  });
+</script>
